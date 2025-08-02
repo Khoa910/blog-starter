7a02041cfa5db0b311ba511f4d9ca2637cc7d158
Khoa910 <khoa9102003@gmail.com>
2025-08-02 15:14:51 +0700

    update

---
+++ src/components/MainCategories.jsx
@@ -0,0 +1,7 @@
+const MainCategories = () => {
+    return (
+        <div className="hidden md:flex bg-white round-3xl xl:rounded-full p-4 shadow-lg items-center justify-center gap-8">MainCategories</div>
+    );
+};
+
+export default MainCategories;  
